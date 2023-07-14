496674f519c2c1f3c498c5869f5781da7c05522f
Muditkumar123 <111898378+Muditkumar123@users.noreply.github.com>
2023-07-14 14:28:35 +0530

    Add files via upload

---
+++ 3. Chapter 3/03_string_functions.py
@@ -0,0 +1,10 @@
+story = "This sentence is used for showing the exsisting fuction of string"
+
+# String Functions
+print(len(story))
+print(story.endswith("string"))
+print(story.count("i"))
+print(story.capitalize())
+print(story.find("for"))
+print(story.replace("exsisting", "existing"))
+ 
